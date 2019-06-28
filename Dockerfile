2c897244069a594a098307e077c83b7c13427e64
Xaroth Brook <xaroth+github@xaroth.nl>
2019-06-28 15:28:40 +0200

    -Add: Initial layout

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM osixia/openldap:1.2.5-dev
+LABEL author="https://github.com/Xaroth"
+
+RUN apt-get -y update \
+    && LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends inotify-tools \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+ADD wait-for-file /wait-for-file
+ADD run-slapd /run-slapd
+ADD process.sh /container/service/slapd/process.sh
+ADD ldif/* /container/service/slapd/assets/config/bootstrap/ldif/
+
+RUN chmod +x /wait-for-file /container/service/slapd/process.sh
+
+ENTRYPOINT ["/wait-for-file", "/container/tool/run"]
---
+++ process.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+trap "{ kill -INT `cat /run/slapd/slapd.pid` || true }" SIGINT SIGTERM EXIT
+
+# set -x (bash debug) if log level is trace
+# https://github.com/osixia/docker-light-baseimage/blob/stable/image/tool/log-helper
+log-helper level eq trace && set -x
+
+# Reduce maximum number of number of open file descriptors to 1024
+# otherwise slapd consumes two orders of magnitude more of RAM
+# see https://github.com/docker/docker/issues/8231
+ulimit -n $LDAP_NOFILE
+
+CONTAINER_SERVICE_DIR="${CONTAINER_SERVICE_DIR:-/container/service}"
+LDAP_TLS_KEY_FILENAME="${LDAP_TLS_KEY_FILENAME:-ldap.key}"
+
+if [ -z "$FILE_TO_WAIT_FOR" ]; then
+    # By default, we'll wait for the privkey file
+    FILE_TO_WAIT_FOR="${CONTAINER_SERVICE_DIR}/slapd/assets/certs/${LDAP_TLS_KEY_FILENAME}"
+fi
+
+while true; do
+    exec /usr/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u openldap -g openldap -d $LDAP_LOG_LEVEL &
+    PID=$!
+    inotifywait "$LDAP_TLS_KEY_FILENAME"
+    kill -INT `cat /run/slapd/slapd.pid` $PID
+done
